67584e1a28e7aa5566395a62334df2756dcd71a3
pranav-bot-code <pkk02008@gmail.com>
2025-05-18 20:58:46 +0530

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -5,28 +5,7 @@ import nextPwa from 'next-pwa'
 const nextConfig = {};
 
 const withPWA = nextPwa({
-  dest: 'public',
-  async headers() {
-    return [
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ]
-  }
+  dest: 'public'
 })
 
 export default withPWA({
